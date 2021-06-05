dbc23a79292794efabbb3c96c22eb66460cf64fb
st127 <tswuyin_st127@163.com>
2021-06-05 21:19:43 +0800

    test: Add tests for Cwb_Buffer

---
+++ Tests/Buffer.test.c
@@ -0,0 +1,56 @@
+/*
+	cwb
+	File:/Test/Buffer.test.c
+	Date:2021.06.06
+	By LGPL v3.0 and Anti-996 License.
+	Copyright(C) 2021 cwb developers.All rights reserved.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdint.h>
+#include<assert.h>
+#include<time.h>
+
+#include<cwb/Buffer.h>
+
+#include"Test.h"
+
+#define TEST_TARGET 100
+
+int main(void)
+{
+	test_init();
+
+	TEST_LOOP(TEST_TARGET) {
+		Cwb_Buffer *buffer = cwb_buffer_new();
+		assert(buffer);
+
+		size_t size = (random()+1)%65536;
+		void *data = test_random_data(size);
+		size_t tmp = size;
+		void *tmpData = data;
+
+		while (size) {
+			size_t step = random();
+			step = step > size ? size : step;
+			assert(cwb_buffer_append(buffer,data,step));
+			data = (void*)((uint8_t*)data + step);
+			size -= step;
+		}
+
+		size = tmp;
+
+		void *result = cwb_buffer_convert(buffer,NULL,0);
+		assert(result);
+
+		assert(!memcmp(result,tmpData,size));
+		
+		free(result);
+		free(tmpData);
+		cwb_buffer_destroy(buffer);
+	}
+
+	return 0;
+}
--- Tests/Test.h
+++ Tests/Test.h
@@ -1,7 +1,7 @@
 /*
 	cwb
 	File:/Tests/Test.h
-	Date:2021.05.02
+	Date:2021.05.23
 	By LGPL v3.0 and Anti-996 License.
 	Copyright(C) 2021 cwb developers.All rights reserved.
 */
@@ -9,6 +9,27 @@
 #ifndef TEST_ERROR_H_INC
 #define TEST_ERROR_H_INC
 
-#define err_log(msg) fprintf(stderr,(msg))
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+static void test_init(void)
+{
+	srand(time(NULL));
+	return;
+}
+
+static void *test_random_data(size_t length)
+{
+	uint8_t *data = malloc(length);
+	assert(data);
+
+	for (size_t count = 0;count < length;count++)
+		data[count] = (uint8_t)random();
+	
+	return data;
+}
+
+#define TEST_LOOP(target) for (int count = 0;count < (target);count++)
 
 #endif
